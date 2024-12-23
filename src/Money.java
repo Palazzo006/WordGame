3f617cd92976e1a349158d0ac9be4b76310ce91b
Palazzo006 <zac2178493@maricopa.edu>
2024-12-23 12:11:40 -0700

    Completed Lesson4

--- src/Money.java
+++ src/Money.java
@@ -5,10 +5,12 @@ public class Money implements Award {
     @Override
     public int displayWinnings(Players player, boolean isWin) {
         if (isWin) {
-            System.out.println(player.getFirstName() + " " + player.getLastName() + ", Winner Winner Chicken Dinner!");
+            // Declare the winning amount
+            System.out.println(player.getFirstName() + " " + player.getLastName() + ", Winner Winner Chicken Dinner! You won $" + WINNING_AMOUNT + "!");
             return (int) WINNING_AMOUNT; // Return positive winning amount
         } else {
-            System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You lost $" + Math.abs(LOSING_AMOUNT));
+            // Declare the losing amount
+            System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You lost $" + Math.abs(LOSING_AMOUNT) + ".");
             return (int) LOSING_AMOUNT; // Return negative losing amount
         }
     }
