47d0c31c96a2b4f33728002dc17803ef9d97c1b9
Palazzo006 <zac2178493@maricopa.edu>
2024-12-23 11:48:35 -0700

    Completed Lesson4: Added multiplayer support and award system

--- src/Physical.java
+++ src/Physical.java
@@ -14,10 +14,10 @@ public class Physical implements Award {
     public int displayWinnings(Players player, boolean isWin) {
         int prizeIndex = getRandomPrize(); // Get a random prize index
         if (isWin) {
-            System.out.println(player.getFirstName() + " " + player.getLastName() + ",you won a " + prizes[prizeIndex] + "!");
+            System.out.println(player.getFirstName() + " " + player.getLastName() + ", you won a " + prizes[prizeIndex] + "!");
             return 0; // No monetary value for physical prizes
         } else {
-            System.out.println(player.getFirstName() + " " + player.getLastName() + ",sorry, that is incorrect! You could have won a " + prizes[prizeIndex] + ".");
+            System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You could have won a " + prizes[prizeIndex] + ".");
             return 0; // No monetary value for physical prizes
         }
     }
