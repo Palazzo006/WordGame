b68064c8d0607d6d55984e8c0a170322999c4a64
Palazzo006 <zac2178493@maricopa.edu>
2024-12-23 11:34:02 -0700

    Completed Lesson4: Added multiplayer support and award system

--- src/Money.java
+++ src/Money.java
@@ -5,7 +5,7 @@ public class Money implements Award {
     @Override
     public int displayWinnings(Players player, boolean isWin) {
         if (isWin) {
-            System.out.println(player.getFirstName() + " " + player.getLastName() + ", Winner Winner, Chicken Dinner!");
+            System.out.println(player.getFirstName() + " " + player.getLastName() + ", Winner Winner Chicken Dinner!");
             return (int) WINNING_AMOUNT;
         } else {
             System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You lost $" + Math.abs(LOSING_AMOUNT));
