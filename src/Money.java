197d6b4f8d1e82bf9560502799402158e81ee5ff
Palazzo006 <zac2178493@maricopa.edu>
2024-12-23 11:35:30 -0700

    Completed Lesson4: Added multiplayer support and award system

--- src/Money.java
+++ src/Money.java
@@ -1,15 +1,15 @@
 public class Money implements Award {
-    private static final double WINNING_AMOUNT = 1000;
-    private static final double LOSING_AMOUNT = -200;
+    private static final double WINNING_AMOUNT = 1000; // Winning amount constant
+    private static final double LOSING_AMOUNT = -200;   // Losing amount constant
 
     @Override
     public int displayWinnings(Players player, boolean isWin) {
         if (isWin) {
             System.out.println(player.getFirstName() + " " + player.getLastName() + ", Winner Winner Chicken Dinner!");
-            return (int) WINNING_AMOUNT;
+            return (int) WINNING_AMOUNT; // Return positive winning amount
         } else {
             System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You lost $" + Math.abs(LOSING_AMOUNT));
-            return (int) LOSING_AMOUNT;
+            return (int) LOSING_AMOUNT; // Return negative losing amount
         }
     }
 }
--- src/Physical.java
+++ src/Physical.java
@@ -1,22 +1,24 @@
 import java.util.Random;
 
 public class Physical implements Award {
+    // Array of physical prizes
     private String[] prizes = {"Car", "Vacation Package", "TV", "Appliances", "Furniture"};
     private Random random = new Random();
 
+    // Method to get a random prize index
     public int getRandomPrize() {
         return random.nextInt(prizes.length);
     }
 
     @Override
     public int displayWinnings(Players player, boolean isWin) {
-        int prizeIndex = getRandomPrize();
+        int prizeIndex = getRandomPrize(); // Get a random prize index
         if (isWin) {
             System.out.println(player.getFirstName() + " " + player.getLastName() + ", you won a " + prizes[prizeIndex] + "!");
-            return 0; 
+            return 0; // No monetary value for physical prizes
         } else {
             System.out.println(player.getFirstName() + " " + player.getLastName() + ", sorry, that is incorrect! You could have won a " + prizes[prizeIndex] + ".");
-            return 0; 
+            return 0; // No monetary value for physical prizes
         }
     }
 }
